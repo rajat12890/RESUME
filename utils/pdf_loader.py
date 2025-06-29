e9ad30cba8e677867c91cb53f5b979d76a53287c
rajat12890 <pariharrajat76@gmail.com>
2025-06-29 21:33:30 +0530

    Create pdf_loader.py

---
+++ utils/pdf_loader.py
@@ -0,0 +1,11 @@
+from langchain.document_loaders import PyPDFLoader
+import tempfile
+
+def extract_text_from_pdf(pdf_file)->str:
+    with tempfile.NamedTemporaryFile(delete=False,suffix=".pdf")as tmp:
+        tmp.write(pdf_file.read())
+        tmp_path=tmp.name
+    loader=PyPDFLoader(tmp_path)
+    documents=loader.load()
+    all_text = "\n".join([doc.page_content for doc in documents])
+    return all_text.strip()
