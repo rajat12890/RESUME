40a946b1d800584c595f4611f00ad62d8578777b
rajat12890 <pariharrajat76@gmail.com>
2025-10-30 21:54:28 +0530

    Update match_score.py

--- chains/match_score.py
+++ chains/match_score.py
@@ -1,16 +1,12 @@
 from langchain_core.prompts import PromptTemplate
-
 from langchain.chains.llm import LLMChain
-
 from langchain_groq import ChatGroq
 
 
 def get_match_score_chain(llm):
-
-
     prompt = PromptTemplate(
-    input_variables=["resume", "job_description"],
-    template="""
+        input_variables=["resume", "job_description"],
+        template="""
 You are an AI recruitment analyst for a top-tier tech company (like Google).
 Analyze the resume against the provided job description and evaluate how well the candidate matches the role.
 
@@ -28,6 +24,6 @@ Provide the following:
 Score: <score out of 100>
 Explanation: <brief, bullet-point summary>
 """
-)
+    )
 
-    return LLMChain(prompt=prompt,llm=llm)
+    return LLMChain(prompt=prompt, llm=llm)
